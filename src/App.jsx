5f66747d7b4454a39ffded0bfe7e80445980d0b5
andrey <andreytorrente@gmail.com>
2022-11-20 23:29:49 -0600

    delete unnecesary container component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import TaskForm from "./components/TaskForm";
-import TasksContainer from "./components/TasksContainer";
 import { useState, useEffect } from "react";
+import Tasks from "./components/Tasks";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
@@ -22,8 +22,16 @@ function App() {
         <TaskForm inputValue={inputValue} setInputValue={setInputValue} tasks={tasks} setTasks={setTasks} />
       </div>
 
-      <div className="row w-100 p-4" style={{ height: "300px" }}>
-        {tasks.length > 0 ? <TasksContainer tasks={tasks} setTasks={setTasks} /> : <p className="text-info text-center p-2 rounded"> Start doing something! </p>}
+      <div className="row w-100">
+        <div className="task-container d-flex flex-column justify-content-space-between gap-4 p-4 overflow-auto" style={{ height: "300px" }}>
+          {tasks.length > 0 ? (
+            tasks.map((task) => {
+              return <Tasks tasks={tasks} setTasks={setTasks} task={task} key={task.id} />;
+            })
+          ) : (
+            <p className="text-info text-center p-2 rounded"> Start doing something! </p>
+          )}
+        </div>
       </div>
     </div>
   );
