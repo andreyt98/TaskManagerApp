75dae1ff1ab44c04749b56fec99f8b8cf7d04ec1
andrey <andreytorrente@gmail.com>
2022-11-23 22:08:05 -0600

    set algorithm to edit task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,7 +1,14 @@
+import { useRef } from "react";
+import { useState, useEffect } from "react";
 const Tasks = ({ tasks, setTasks, task }) => {
-  const deleteTask = () => {
-    let newA;
+  const [showEditable, setShowEditable] = useState(false);
+  const [editableValue, setEditableValue] = useState("");
+  const [newArray, setNewArray] = useState([]);
 
+  const editableTaskRef = useRef();
+
+  const deleteTask = (event) => {
+    let newA;
     tasks.forEach((e) => {
       if (e.id == task.id) {
         newA = tasks.filter((el) => el.id != task.id);
@@ -12,7 +19,10 @@ const Tasks = ({ tasks, setTasks, task }) => {
         const index = tasks.indexOf(e);
         tasks.splice(index, 1);
 
-        setTasks(newA);
+        event.target.parentElement.parentElement.style.transform = "scale(0.8)";
+        setTimeout(() => {
+          setTasks(newA);
+        }, 200);
       }
     });
   };
@@ -28,12 +38,78 @@ const Tasks = ({ tasks, setTasks, task }) => {
     setTasks(completedTodo);
   };
 
+  useEffect(() => {
+    setNewArray([...tasks]);
+  }, []);
+
+  function showEditableInput() {
+    setShowEditable(!showEditable);
+
+    setTimeout(() => {
+      editableTaskRef.current.select();
+    }, 0);
+
+    edit();
+  }
+
+  function edit() {
+    if (showEditable) {
+      newArray.forEach((el) => {
+        if (el.id == task.id && editableValue !== "") {
+          el.description = editableValue;
+
+          localStorage.clear();
+          localStorage.setItem("tasks", JSON.stringify(newArray));
+          setTasks(newArray);
+          setEditableValue("")
+        }
+      });
+    }
+  }
+
+  function handleEnterKey(e) {
+    if (e.key === "Enter") {
+      setShowEditable(!showEditable);
+      edit();
+    }
+  }
   return (
-    <div className={"task row rounded py-3 border-5 border-primary border-start d-flex align-items-center justify-content-space-between " + (task.completed ? "completed border-success": "")}>
-      <div className={"w-auto "  + (task.completed ? "text-decoration-line-through text-muted" :"")}>{task.description}</div>
+    <div className={"task row rounded py-3 border-5 border-primary border-start d-flex align-items-center justify-content-space-between " + (task.completed ? "completed border-success" : "")}>
+      <div className={"w-auto " + (task.completed ? "text-decoration-line-through text-muted" : "")}>{task.description}</div>
+
+      <input
+        value={editableValue}
+        onChange={(e) => {
+          setEditableValue(e.target.value);
+        }}
+        type="text"
+        name=""
+        id="new"
+        style={{ display: showEditable ? "block" : "none", backgroundColor: "#073056", color: "white", border: "none" }}
+        ref={editableTaskRef}
+        placeholder="editing task..."
+        onKeyPress={(e) => {
+          handleEnterKey(e);
+        }}
+      />
+
       <div className="w-auto ms-auto ">
-        <input type="checkbox" className="" name="" id="" onClick={checkTask} defaultChecked={task.completed} />
-        <span role="button"  className="ms-3" onClick={deleteTask}>🗑️</span>
+        <i className={showEditable ? "bi bi-clipboard2-check me-3" : "bi bi-pencil-fill me-3"} role={"button"} onClick={showEditableInput}></i>
+        <i
+          className="bi bi-trash3-fill me-3"
+          role="button"
+          onClick={(e) => {
+            deleteTask(e);
+          }}
+        ></i>
+        <i
+          className={task.completed ? "text-primary bi bi-check-circle" : "bi bi-check-circle "}
+          role="button"
+          onClick={(e) => {
+            checkTask(e);
+          }}
+          defaultChecked={task.completed}
+        ></i>
       </div>
     </div>
   );
